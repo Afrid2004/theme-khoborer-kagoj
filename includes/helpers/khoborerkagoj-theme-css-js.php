a87cf9f58a52f4ac58331392161cee0f7d758890
afrid2004 <mdfaisalafrid@gmail.com>
2025-10-08 20:33:15 +0600

    code structured

--- includes/helpers/khoborerkagoj-theme-css-js.php
+++ includes/helpers/khoborerkagoj-theme-css-js.php
@@ -2,12 +2,13 @@
 
 function media_theme_css_js(): void {
     // CSS Files
-    wp_enqueue_style('bootstrap-min-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', array(), '5.0.2', 'all');
-    wp_enqueue_style('font-awesome-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css', array(), '7.0.1', 'all');
-    wp_enqueue_style('swiper-bundle-min-css', 'https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css', array(), '12.0.1', 'all');
-    wp_enqueue_style('lightgallery-bundle-min-css', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.8.3/css/lightgallery-bundle.min.css', array(), '2.8.3', 'all');
-    wp_enqueue_style('school-theme-style-css',get_stylesheet_directory_uri() . '/assets/css/style.css',array('lightgallery-bundle-min-css'),filemtime(get_stylesheet_directory() . '/assets/css/style.css'),'all');
-    
+    wp_enqueue_style('bootstrap-min-css-5-3-7', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css', array(), '5.3.7', 'all');
+    wp_enqueue_style('bootstrap-min-css-5-3-0', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css', array(), '5.3.0', 'all');
+    wp_enqueue_style('bootstrap-icon-min-css', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css', array(), '1.13.1', 'all');
+    wp_enqueue_style('font-awesome-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css', array(), '6.7.2', 'all');
+    wp_enqueue_style('khoborerkagoj-theme-style-css',get_stylesheet_directory_uri() . '/assets/css/style.css',array(),'all');
+    wp_enqueue_style('khoborerkagoj-theme-style-css',get_stylesheet_directory_uri() . '/assets/css/responsive.css',array(),'all');
+
     // JS Files
     wp_enqueue_script('popper-min-js', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js', array('jquery'), '2.9.2', true);
     wp_enqueue_script('bootstrap-min-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js', array('jquery'), '5.0.2', true);
